2138315ed61ea427284a3abf1c3a72c7108a1ed1
Mehmed Tukulić <mehmed.tukulic@hotmail.com>
2019-11-05 23:41:23 +0100

    Installed mongojs,body-parser, first rest api

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongojs = require('mongojs');
+const db = mongojs('mongodb+srv://mehmedtukulic:test123@cluster0-jqb5e.mongodb.net/ShishaTime?retryWrites=true&w=majority')
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 app.use('/',(req,res,next)=>{
     console.log('New visit from :'+ req.ip);
     next();
@@ -12,5 +19,14 @@ app.get('/',(req,res)=>{
 });
 
 app.listen(port,()=>{
-    console.log('Server listening on'+ port);
+    console.log('Server listening on port : '+ port);
+});
+
+app.get('/users', (req,res) => {
+   db.users.find({} , (error,docs) => {
+       if(error){
+           throw error;
+       }
+       res.json(docs);
+   });
 });
