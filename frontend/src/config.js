d948c5328bb877e390de28d24706dd5c11b1a5f6
Mehmed Tukulic <mehmed.tukulic@hotmail.com>
2020-01-16 15:15:35 +0100

    heroku fix 2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 let baseUrl;
-if (!process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:3001'
 } else {
-    baseUrl = 'http://shisha-time.herokuapp.com/'
+    baseUrl = 'https://shisha-time.herokuapp.com/'
 }
 
 module.exports = {
